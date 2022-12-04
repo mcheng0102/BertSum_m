b3df976a2e62924a36ac0590e7d86b685ea9ce96
mcheng0102 <91365155+mcheng0102@users.noreply.github.com>
2022-12-04 22:07:00 +0800

    Update trainer.py

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -291,10 +291,10 @@ class Trainer(object):
                             save_pred.write(pred[i].strip()+'\n')
                             
         logger.info('End testing')
-        if(step!=-1 and self.args.report_rouge):
-            rouges = test_rouge(self.args.temp_dir, can_path, gold_path)
-            logger.info('Rouges at step %d \n%s' % (step, rouge_results_to_str(rouges)))
-        self._report_step(0, step, valid_stats=stats)
+        #         if(step!=-1 and self.args.report_rouge):
+        #             rouges = test_rouge(self.args.temp_dir, can_path, gold_path)
+        #             logger.info('Rouges at step %d \n%s' % (step, rouge_results_to_str(rouges)))
+        #         self._report_step(0, step, valid_stats=stats)
 
         return stats
 
