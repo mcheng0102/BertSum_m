579e00968a2766aa44e14b0d7089a75559daa249
mcheng0102 <91365155+mcheng0102@users.noreply.github.com>
2022-12-04 19:34:04 +0800

    Update trainer.py

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -247,30 +247,24 @@ class Trainer(object):
                         mask = batch.mask
                         mask_cls = batch.mask_cls
 
-
                         gold = []
                         pred = []
 
-                        if (cal_lead):
-                            selected_ids = [list(range(batch.clss.size(1)))] * batch.batch_size
-                        elif (cal_oracle):
-                            selected_ids = [[j for j in range(batch.clss.size(1)) if labels[i][j] == 1] for i in
-                                            range(batch.batch_size)]
-                        else:
-                            sent_scores, mask = self.model(src, segs, clss, mask, mask_cls)
-                            
-                            if labels.shape != sent_scores.shape:
-                                labels = labels.reshape(sent_scores.shape)
-                                
-                            loss = self.loss(sent_scores, labels.float())
-                            loss = (loss * mask.float()).sum()
-                            batch_stats = Statistics(float(loss.cpu().data.numpy()), len(labels))
-                            stats.update(batch_stats)
+                        sent_scores, mask = self.model(src, segs, clss, mask, mask_cls)
 
-                            sent_scores = sent_scores + mask.float()
-                            sent_scores = sent_scores.cpu().data.numpy()
-                            selected_ids = np.argsort(-sent_scores, 1)
+                        if labels.shape != sent_scores.shape:
+                            labels = labels.reshape(sent_scores.shape)
+
+                        loss = self.loss(sent_scores, labels.float())
+                        loss = (loss * mask.float()).sum()
+                        batch_stats = Statistics(float(loss.cpu().data.numpy()), len(labels))
+                        stats.update(batch_stats)
+
+                        sent_scores = sent_scores + mask.float()
+                        sent_scores = sent_scores.cpu().data.numpy()
+                        selected_ids = np.argsort(-sent_scores, 1)
                         # selected_ids = np.sort(selected_ids,1)
+                        
                         for i, idx in enumerate(selected_ids):
                             _pred = []
                             if(len(batch.src_str[i])==0):
