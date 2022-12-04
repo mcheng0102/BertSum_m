79e7a389dcbe6714b7a70c930c57b9271691095a
mcheng0102 <91365155+mcheng0102@users.noreply.github.com>
2022-12-04 21:55:46 +0800

    Update trainer.py

--- src/models/trainer.py
+++ src/models/trainer.py
@@ -261,10 +261,11 @@ class Trainer(object):
 
                         sent_scores = sent_scores + mask.float()
                         sent_scores = sent_scores.cpu().data.numpy()
-                        #selected_ids = np.argsort(-sent_scores, 1)
-                        selected_ids = np.array([i for i in range(len(sent_scores))])
+                        selected_ids = np.argsort(-sent_scores, 1)
+                        #selected_ids = np.array([i for i in range(len(sent_scores))])
                         # selected_ids = np.sort(selected_ids,1)
                         logger.info(len(selected_ids))
+                        logger.info(selected_ids)
                         for i, idx in enumerate(selected_ids):
                             _pred = []
                             if(len(batch.src_str[i])==0):
